b289f7048be1cb756e40a1e6242a3dfa22bcd464
naerim <naerim1119@gmail.com>
2020-08-29 14:09:25 +0900

    [ADD] using styled-componens

--- next_practice/pages/page1.js
+++ next_practice/pages/page1.js
@@ -1,12 +1,18 @@
 import Head from "next/head";
 import Icon from "../static/icon.png";
 import { add } from "../src/util";
+import styled from "styled-components";
+
+const MyP = styled.div`
+  color: blue;
+  font-size: 18pt;
+`;
 
 const Page1 = () => {
   return (
     <div>
-      <p>{`10 + 20 = ${add(10, 20)}`}</p>
-      <p>This is home page</p>
+      <MyP>{`10 + 20 = ${add(10, 20)}`}</MyP>
+      <MyP>This is home page</MyP>
       <img src={Icon} />
       <Head>
         <title>page1</title>
@@ -14,14 +20,6 @@ const Page1 = () => {
       <Head>
         <meta name="description" content="hello world" />
       </Head>
-      <style jsx>
-        {`
-          p {
-            color: blue;
-            font-size: 18pt;
-          }
-        `}
-      </style>
     </div>
   );
 };
