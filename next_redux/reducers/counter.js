029f9eae1500e995b7f9d27cdb9ba4a76a8072df
naerim <naerim1119@gmail.com>
2020-09-18 14:19:38 +0900

    [ADD] rootReducer, counter reducer

---
+++ next_redux/reducers/counter.js
@@ -0,0 +1,35 @@
+import produce from "immer";
+
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+export const increase = (input) => ({
+  type: INCREASE,
+  input,
+});
+
+export const decrease = (input) => ({
+  type: DECREASE,
+  input,
+});
+
+const initialState = {
+  number: 0,
+};
+
+function counter(state = initialState, action) {
+  return produce(state, (draft) => {
+    switch (action.type) {
+      case INCREASE:
+        draft.number = state.number + action.input;
+        break;
+      case DECREASE:
+        draft.number = state.number - action.input;
+        break;
+      default:
+        break;
+    }
+  });
+}
+
+export default counter;
