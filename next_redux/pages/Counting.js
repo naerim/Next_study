9e8ac2ff85c1ef9ca8e672f2ce413debb62e8246
naerim <naerim1119@gmail.com>
2020-09-21 21:56:12 +0900

    [ADD] Counting

---
+++ next_redux/pages/Counting.js
@@ -0,0 +1,38 @@
+import React, { useCallback, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increase, decrease } from "../reducers/counter";
+import Layout from "../components/Layout";
+
+const Counting = () => {
+  const { countNum } = useSelector((state) => ({
+    constNum: state.counter.number,
+  }));
+
+  const dispatch = useDispatch();
+  const onIncrease = useCallback((input) => dispatch(increase(input)), [
+    dispatch,
+  ]);
+  const onDecrease = useCallback((input) => dispatch(decrease(input)), [
+    dispatch,
+  ]);
+
+  const [input, setInput] = useState("");
+
+  return (
+    <Layout>
+      <div>
+        <h3>Counting Number</h3>
+        <h1>{countNum}</h1>
+        <input
+          value={input}
+          placeholder="숫자를 입력하세요"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={onIncrease(Number(input))}>+</button>
+        <button onClick={onDecrease(Number(input))}>-</button>
+      </div>
+    </Layout>
+  );
+};
+
+export default Counting;
