befe56df8a6bea1c7d0ae342dc7a8ff3ad266d29
naerim <naerim1119@gmail.com>
2020-09-27 21:22:35 +0900

    [ADD] menu

--- next_redux/pages/_app.js
+++ next_redux/pages/_app.js
@@ -1,9 +1,31 @@
 import wrapper from "../store";
 import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
+
+const Menu = styled.div`
+  margin: 20px;
+`;
+
+const LinkStyle = styled.a`
+  margin: 10px;
+`;
 
 function App({ Component, pageProps }) {
   return (
     <div>
+      <Menu>
+        <Link href="/">
+          <LinkStyle>Home</LinkStyle>
+        </Link>
+        <Link href="/Counting">
+          <LinkStyle>Count</LinkStyle>
+        </Link>
+        <Link href="/TodoList">
+          <LinkStyle>Todo</LinkStyle>
+        </Link>
+      </Menu>
+
       <Component {...pageProps} />
     </div>
   );
