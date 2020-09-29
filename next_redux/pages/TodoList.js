e853e7e6d2e7b60e41dc4237e553f4b68fdcf0ce
naerim <naerim1119@gmail.com>
2020-09-29 12:38:38 +0900

    [ADD] TodoList

--- next_redux/pages/TodoList.js
+++ next_redux/pages/TodoList.js
@@ -1,5 +1,7 @@
 import React, { useCallback, useState } from "react";
 import Layout from "../components/Layout";
+import { insert, check, remove } from "../reducers/todos";
+import { useDispatch, useSelector } from "react-redux";
 
 const TodoItem = ({ todo, onCheck, onRemove }) => {
   return (
@@ -19,11 +21,42 @@ const TodoItem = ({ todo, onCheck, onRemove }) => {
 };
 
 const TodoList = () => {
+  const [todoInput, setTodoInput] = useState("");
+  const { todos } = useSelector(({ todos }) => ({
+    todos: todos.todos,
+  }));
+
+  const dispatch = useDispatch();
+  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
+  const onCheck = useCallback((id) => dispatch(check(id)), [dispatch]);
+  const onRemove = useCallback((id) => dispatch(remove(id)), [dispatch]);
+
+  // 등록 버튼을 눌렀을 때, todos배열에 추가되고 input이 초기화됨
+  const onSubmit = (e) => {
+    e.preventDefault();
+    onInsert(todoInput);
+    setTodoInput("");
+  };
+
+  const onChange = (e) => setTodoInput(e.target.value);
+
   return (
     <Layout>
       <div>
-        <input value="input" />
-        <button type="button">등록</button>
+        <input value={todoInput} onChange={onChange} />
+        <button type="button " onClick={onSubmit}>
+          등록
+        </button>
+      </div>
+      <div>
+        {todos.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            onCheck={onCheck}
+            onRemove={onRemove}
+          />
+        ))}
       </div>
     </Layout>
   );
